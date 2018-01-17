678d386ced30b2947ce995f8c1fe6503873d3960
casey.hood <caseyhsoccer@gmail.com>
2018-01-17 00:14:24 -0800

    promises

--- index.js
+++ index.js
@@ -7,11 +7,20 @@ const prefix = config.PREFIX;
 var bot = new Discord.Client();
 
 var play = function(connection, message) {
+
   var server = servers[message.guild.id];
-  console.log(server.queue[0]);
+  //console.log(server.queue[0]);
   // ?const streamOptions = { seek: 0, volume: 1 };
-  console.log(server.queue);
+  //console.log(server.queue);
+  //bot.user.setPresence({game:{name: 'IDLE', type: 2}}).then(function(){console.log("info.title");}).catch(function(err){throw err;});
+
   server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+    
+  ytdl.getInfo(server.queue[0], function(err, info){
+    if(err) throw err;
+    bot.user.setPresence({game:{name: info.title.toUpperCase(), type: 2}}).then(function(){console.log(info.title.toUpperCase());}).catch(function(err){throw err;});
+
+  })
   // const stream = ytdl('https://www.youtube.com/watch?v=XAWgeLF9EVQ', { filter : 'audioonly' });
   //     const dispatcher = connection.playStream(stream, streamOptions);
   //     console.log(stream);
@@ -22,7 +31,7 @@ var play = function(connection, message) {
       play(connection, message);
     }
     else {
-      connection.disconnect();
+      bot.user.setPresence({game:{name: 'NADA...', type: 2}}).then(function(){connection.disconnect();console.log("now stopped and disconnected");}).catch(function(err){throw err;});
     }
   })
   // return;
@@ -33,6 +42,8 @@ var servers = {};
 bot.on("ready", function(){
   console.log("MusicBot Online...");
   console.log(prefix);
+  bot.user.setPresence({game:{name: 'NADA...', type: 2}}).catch(function(err){throw err;});
+
 })
 
 bot.on("message", async function(message) {
@@ -48,6 +59,15 @@ bot.on("message", async function(message) {
   //if(!message.content.startsWith(prefix)) return;
 
   switch(args[0].toLowerCase()) {
+    case "invite":
+      bot.generateInvite(["ADMINISTRATOR"]).then(function(link){
+        console.log(link);
+        message.channel.send("<"+link+">");
+      }).catch(function(err){
+        throw err;
+      });
+      break;
+
     case "play":
       if(!args[1]){
         message.channel.send("Please provide a link");
@@ -68,6 +88,7 @@ bot.on("message", async function(message) {
 
       if(!message.guild.voiceConnection){
         message.member.voiceChannel.join().then(function(connection){
+
           play(connection, message);
           // console.log(message);
         }).catch(function(err){console.log(err.stack)});
@@ -84,8 +105,9 @@ bot.on("message", async function(message) {
     case "stop":
       var server = servers[message.guild.id];
       if(message.guild.voiceConnection){
-        message.guild.voiceConnection.disconnect();
-        console.log("now stopped and disconnected");
+
+        bot.user.setPresence({game:{name: 'NADA...', type: 2}}).then(function(){message.guild.voiceConnection.disconnect();message.channel.send(":wave:");}).catch(function(err){throw err;});
+
       }
       break;
   }
